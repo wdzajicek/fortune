56d5d70944ecdcec3f35d29c4b91ddfb9b3f5b0a
wdzajicek <tig.welder4@gmail.com>
2026-02-06 11:48:51 -0600

    Switch model used.

--- netlify/functions/fortune.js
+++ netlify/functions/fortune.js
@@ -11,7 +11,7 @@ export const handler = async (event, context) => {
   }
 
   const genAI = new GoogleGenerativeAI(key);
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
   try {
     console.log("Calling Gemini API...");
